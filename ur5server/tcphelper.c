20edea9a4301edfcbcda124d4ae919a13aade080
ahardstylec <andreas.collmann@gmail.com>
2014-01-09 15:55:58 +0100

    parse options

--- ur5server/tcphelper.c
+++ ur5server/tcphelper.c
@@ -1,34 +1,29 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
 #include <errno.h>
 
 char * get_interface_addr(char *interface, unsigned long *ip_addr){
-	// get ip addr of interface eth0
-	
+	// get ip addr of interface
 
 	struct ifreq ifr;
-	size_t if_name_len=strlen(interface);
-	if (if_name_len<sizeof(ifr.ifr_name)) {
-	    memcpy(ifr.ifr_name,interface,if_name_len);
-	    ifr.ifr_name[if_name_len]=0;
-	} else {
-	    error("interface name is too long");
-	}
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	int fd=socket(AF_INET,SOCK_DGRAM,0);
-	if (fd==-1) {
-	    error(strerror(errno));
-	}
-
-	if (ioctl(fd, SIOCGIFADDR, &ifr)==-1) {
-	    error("interface not found");
-	    close(fd);
-	    exit(0);
-	}
-
+    //Type of address to retrieve - IPv4 IP address
+    ifr.ifr_addr.sa_family = AF_INET;
+ 
+    //Copy the interface name in the ifreq structure
+    strncpy(ifr.ifr_name , interface , IFNAMSIZ-1);
+ 
+    if(ioctl(fd, SIOCGIFADDR, &ifr) == -1){
+    	error("interface not found or offline");
+    	close(fd);
+		exit(1);
+	}    
+	close(fd);
+ 
 	struct sockaddr_in* ipaddr = (struct sockaddr_in*)&ifr.ifr_addr;
 	*ip_addr= ipaddr->sin_addr.s_addr;
-	printf("IP address: %s\n", inet_ntoa(((struct sockaddr_in *)&ipaddr)->sin_addr));
-	return 0;
+	return inet_ntoa((ipaddr->sin_addr));
 }
--- ur5server/ur5-server.c
+++ ur5server/ur5-server.c
@@ -16,7 +16,7 @@ void error(const char *msg)
     exit(1);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int sockfd, newsockfd, portno;
 	unsigned long * ip_addr;
@@ -24,23 +24,53 @@ int main(int argc, char *argv[])
 	char buffer[256];
 	struct sockaddr_in serv_addr, cli_addr;
 	int n;
-	switch(argc){
-		case 1:
-			*ip_addr = inet_addr("127.0.0.1");
-			if(*ip_addr == -1){
-				exit(0);
-			}
-			portno=8000;
-			break;
-		case 2:
-			get_interface_addr(argv[1], ip_addr);
-			portno = 8000;
-			break;
-		case 3:
-			get_interface_addr(argv[1], ip_addr);
-			portno = atoi(argv[2]);
-			break;
+	char * ip_addr_s= "";
+	int iflag = 0;
+	int port=8000;
+	char * interface = "";
+	int c;
+   	opterr = 0;
+
+	while ((c = getopt (argc, argv, "p:i:")) != -1)
+        switch (c)
+           {
+            case 'p':
+            	port = atoi(optarg);
+            	break;
+            case 'i':
+            	iflag=1;
+             	interface = optarg;
+            	break;
+           	case '?':
+             	if (optopt == 'p' || optopt == 'i')
+               		fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+             	else if (isprint (optopt))
+               		fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+            	else
+               		fprintf (stderr, "Unknown option character `\\x%x'.\n", optopt);
+             	return 1;
+           	default:
+           		abort ();
+           }
+     
+    if(iflag == 0){
+    	if(optind < argc){
+			ip_addr_s = argv[optind];
+    	}else{
+    		interface ="lo";
+    		ip_addr_s ="127.0.0.1";
+    	}
+    	if(inet_addr(ip_addr_s) == -1){
+    		printf("ip address wrong");
+    		exit(1);
+    	}
+    	*ip_addr= inet_addr(ip_addr_s);
+    }else{
+    	ip_addr_s = get_interface_addr(interface, ip_addr);
 	}
+
+ 	printf ("port = %d, interface = %s, ip = %s\n", port, interface, ip_addr_s);
+ 	return 0;
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0){
 		error("ERROR opening socket");
@@ -55,14 +85,14 @@ int main(int argc, char *argv[])
 		printf("ERROR on binding on port %i", portno);
 		exit(1);
 	}
-	char * ip_addr_s = inet_ntoa(((struct sockaddr_in *) &serv_addr)->sin_addr);
  	printf("Server is running. listen on %s:%i\n", ip_addr_s, portno);
+
 	return 0;
 	listen(sockfd,5);
 	//while(1) {
 		clilen = sizeof(cli_addr);
 		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-		if (newsockfd < 0) 
+		if (newsockfd < 0)
 			error("ERROR on accept");
 
 		bzero(buffer,256);
@@ -74,5 +104,5 @@ int main(int argc, char *argv[])
 		close(newsockfd);
 		close(sockfd);
 	//}
-	return 0; 
+	return 0;
 }
